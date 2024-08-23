2151a897958de2c53e7428ec0f6d6fa4241dfc17
Gitionia <ben.boettcher@online.de>
2024-08-23 11:38:41 +0200

    cleanup main

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,14 +1,9 @@
 #include "Application.h"
 
-
-#include <iostream>
-
-
 int main() {
 	auto* app = new Application({});
 	app->run();
 	delete app;
 
-	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
