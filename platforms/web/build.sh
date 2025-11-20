6f3cfe81f2cf62d71dbdcdb2156c7eacdc7685e2
Gitionia <ben.boettcher@online.de>
2025-11-20 16:23:36 +0100

    Fixed build.sh, also removed library.web.a

--- platforms/web/build.sh
+++ platforms/web/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+mkdir build
 cd build
-cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=/home/ben/Tools/RaylibForWeb/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM=Web -O3
+cmake ../../../ -DCMAKE_TOOLCHAIN_FILE=~/Tools/RaylibForWeb/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM=Web
 make
 
 mv BubbleBobbleRaylib.html index.html
