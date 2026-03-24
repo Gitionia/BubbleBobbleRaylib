76b91769fca04a55e464e3de165a01f8dd306975
Gitionia <mail@ben-boettcher.de>
2026-03-24 20:37:11 +0100

    Implemented picking up items

--- src/behavior/ItemPickupBehaviorSystem.cpp
+++ src/behavior/ItemPickupBehaviorSystem.cpp
@@ -1,4 +1,21 @@
 #include "ItemPickupBehaviorSystem.h"
+#include "Physics.h"
 
 void ItemPickupBehaviorSystem::Update() {
+    auto dragonView = registry.view<Position, DragonTag>(entt::exclude<DragonHitComponent>);
+    auto itemView = registry.view<Position, ItemTag>(entt::exclude<DragonHitComponent>);
+
+    for (auto dragon : dragonView) {
+        auto [dragonPos] = dragonView.get(dragon);
+
+        for (auto item : itemView) {
+            auto [itemPos] = itemView.get(item);
+
+            if (overlaps(dragonPos, Colliders::fullActorCollider, 
+                itemPos, Colliders::fullActorCollider)) {
+                PRINT_INFO("Picked up Item. 500 Points!");
+                Destroy(item);
+            }
+        }
+    }
 }
