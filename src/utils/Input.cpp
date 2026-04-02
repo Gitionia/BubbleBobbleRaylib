d1d03f3378017c8ea9c87410f4b30a4b5b995b7d
Gitionia <mail@ben-boettcher.de>
2026-04-03 01:02:06 +0200

    Fixed layout in level14 and fixed some input issues for gamepad

--- src/utils/Input.cpp
+++ src/utils/Input.cpp
@@ -5,7 +5,7 @@
 static std::shared_ptr<InputSimulator> inputSimulator;
 
 void Input::Init(std::shared_ptr<InputSimulator> _inputSimulator) {
-    SetGamepadMappings("0300000032150000290a000001010000,Razer Wolverine V2,a:b0,b:b1,x:b2,y:b3,back:b6,guide:b8,start:b7,leftstick:b9,rightstick:b10,leftshoulder:b4,rightshoulder:b5,dpup:h0.1,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,leftx:a0,lefty:a1,rightx:a3,righty:a4,lefttrigger:a2,righttrigger:a5,crc:3361");
+    // SetGamepadMappings("0300000032150000290a000001010000,Razer Wolverine V2,a:b0,b:b1,x:b2,y:b3,back:b6,guide:b8,start:b7,leftstick:b9,rightstick:b10,leftshoulder:b4,rightshoulder:b5,dpup:h0.1,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,leftx:a0,lefty:a1,rightx:a3,righty:a4,lefttrigger:a2,righttrigger:a5,crc:3361");
     ::inputSimulator = _inputSimulator;
 }
 
@@ -22,7 +22,7 @@ bool Input::IsKeyDown(Key key) {
     case Key::Jump:
         return inputSimulator->IsKeyDown(KEY_SPACE) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_RIGHT_FACE_DOWN);
     case Key::Fire:
-        return inputSimulator->IsKeyDown(KEY_A) || IsGamepadButtonPressed(0, GAMEPAD_BUTTON_RIGHT_FACE_LEFT);
+        return inputSimulator->IsKeyDown(KEY_A) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_RIGHT_FACE_LEFT);
     case Key::Any:
         return GetKeyPressed() != 0;
     default:
