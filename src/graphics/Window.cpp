7ed735f98578bf468c9d9730f065f0054f4aaa47
Gitionia <mail@ben-boettcher.de>
2026-04-09 15:48:25 +0200

    Emscripten now starts in fullscreen

--- src/graphics/Window.cpp
+++ src/graphics/Window.cpp
@@ -1,8 +1,27 @@
 #include "Window.h"
 
+#ifdef PLATFORM_WEB
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+
+EM_BOOL emscripten_window_resized_callback(int eventType, const void *reserved, void *userData){
+	return true;
+}
+#endif
+
 Window::Window(int width, int height, const char *title) : width(width), height(height), title(title) {
     SetTraceLogLevel(LOG_ERROR);
     SetTargetFPS(60);
+
+    // Starts web build in soft fullscreen
+#ifdef PLATFORM_WEB
+    EmscriptenFullscreenStrategy strategy;
+    strategy.scaleMode = EMSCRIPTEN_FULLSCREEN_CANVAS_SCALE_STDDEF;
+    strategy.filteringMode = EMSCRIPTEN_FULLSCREEN_FILTERING_DEFAULT;
+    strategy.canvasResizedCallback = emscripten_window_resized_callback;
+    strategy.canvasResizedCallbackUserData = nullptr;
+    emscripten_enter_soft_fullscreen("canvas", &strategy);
+#endif
 }
 
 Window::~Window() {
